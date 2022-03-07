bb0262c11296d1d392e55889a3a724d3840fa847
Lwin-Moe-Aung <lwinmoeaung.ycc@gmail.com>
2022-03-07 23:12:01 +0630

    voucher

--- resources/js/Pages/PosPanel/Pos/Voucher1.vue
+++ resources/js/Pages/PosPanel/Pos/Voucher1.vue
@@ -22,12 +22,28 @@
                         <v-list-item-subtitle class="mt-1">
                             {{ form.item_description }}
                         </v-list-item-subtitle>
-                        <strong class="mt-3"> 1,700,000 .kyats </strong>
+                        <strong class="mt-3">
+                            {{ $page.props.daily_setup[15].kyat }}
+                            .kyats
+                        </strong>
                     </v-list-item-content>
-
-                    <i class="fas fa-save" style="margin-top: -97px"></i>
                 </v-list-item>
-
+                <v-list-item
+                    style="
+                        min-height: 5px !important;
+                        margin-top: -10px;
+                        margin-bottom: 13px;
+                    "
+                >
+                    <v-row style="margin-left: 705px">
+                        <i
+                            class="fas fa-solid fa-cart-plus"
+                            style="padding-right: 12px; color: gold"
+                            @click="addToCart()"
+                        ></i>
+                        <i class="fas fa-save" style="color: gold"></i>
+                    </v-row>
+                </v-list-item>
                 <v-card-actions>
                     <!--new-->
                     <v-form>
@@ -71,7 +87,7 @@
                                                 dense
                                             ></v-text-field>
                                         </v-col>
-                                        <v-col cols="12" sm="6" md="2">
+                                        <v-col cols="12" sm="6" md="3">
                                             <v-text-field
                                                 v-model="form.gold_price"
                                                 label="ကျသင့်ငွေ"
@@ -117,8 +133,9 @@
                                                 dense
                                             ></v-text-field>
                                         </v-col>
-                                        <v-col cols="12" sm="6" md="2">
+                                        <v-col cols="12" sm="6" md="3">
                                             <v-text-field
+                                                v-model="form.gem_price"
                                                 label="ကျသင့်ငွေ"
                                                 placeholder="ကျသင့်ငွေ"
                                                 outlined
@@ -162,8 +179,9 @@
                                                 dense
                                             ></v-text-field>
                                         </v-col>
-                                        <v-col cols="12" sm="6" md="2">
+                                        <v-col cols="12" sm="6" md="3">
                                             <v-text-field
+                                                v-model="form.fee_price"
                                                 label="ကျသင့်ငွေ"
                                                 placeholder="ကျသင့်ငွေ"
                                                 outlined
@@ -182,11 +200,11 @@
                                         <v-col cols="12" sm="6" md="2"> </v-col>
                                         <v-col cols="12" sm="6" md="2"> </v-col>
 
-                                        <v-col cols="12" sm="6" md="2">
+                                        <v-col cols="12" sm="6" md="3">
                                             <v-text-field
                                                 v-model="form.fee_for_making"
-                                                label="kyats"
-                                                placeholder="kyat"
+                                                label="ကျသင့်ငွေ"
+                                                placeholder="ကျသင့်ငွေ"
                                                 outlined
                                                 dense
                                             ></v-text-field>
@@ -201,33 +219,37 @@
                                         </v-list-item-content>
                                         <v-col cols="12" sm="6" md="2">
                                             <v-text-field
-                                                label="kyats"
-                                                placeholder="kyat"
+                                                v-model="form.total_kyat"
+                                                label="ကျပ်"
+                                                placeholder="ကျပ်"
                                                 outlined
                                                 dense
                                             ></v-text-field>
                                         </v-col>
                                         <v-col cols="12" sm="6" md="2">
                                             <v-text-field
-                                                label="kyats"
-                                                placeholder="kyat"
+                                                v-model="form.total_pal"
+                                                label="ပဲ"
+                                                placeholder="ပဲ"
                                                 outlined
                                                 dense
                                             ></v-text-field>
                                         </v-col>
                                         <v-col cols="12" sm="6" md="2">
                                             <v-text-field
-                                                label="kyats"
-                                                placeholder="kyat"
+                                                v-model="form.total_yway"
+                                                label="ရွေး"
+                                                placeholder="ရွေး"
                                                 outlined
                                                 dense
                                             ></v-text-field>
                                         </v-col>
 
-                                        <v-col cols="12" sm="6" md="2">
+                                        <v-col cols="12" sm="6" md="3">
                                             <v-text-field
-                                                label="kyats"
-                                                placeholder="kyat"
+                                                v-model="form.total_before"
+                                                label="ကျသင့်ငွေ"
+                                                placeholder="ကျသင့်ငွေ"
                                                 outlined
                                                 dense
                                             ></v-text-field>
@@ -249,6 +271,7 @@
                                         </v-list-item-content>
                                         <v-col cols="12" sm="6" md="7">
                                             <v-text-field
+                                                v-model="form.item_discount"
                                                 label="kyats"
                                                 placeholder="kyats"
                                                 outlined
@@ -264,6 +287,7 @@
                                         </v-list-item-content>
                                         <v-col cols="12" sm="12" md="7">
                                             <v-text-field
+                                                v-model="form.total_after"
                                                 label="kyats"
                                                 placeholder="kyats"
                                                 outlined
@@ -279,6 +303,7 @@
                                         </v-list-item-content>
                                         <v-col cols="12" sm="12" md="7">
                                             <v-text-field
+                                                v-model="form.paid_money"
                                                 label="kyats"
                                                 placeholder="kyats"
                                                 outlined
@@ -294,6 +319,7 @@
                                         </v-list-item-content>
                                         <v-col cols="12" sm="12" md="7">
                                             <v-text-field
+                                                v-model="form.credit_money"
                                                 label="kyats"
                                                 placeholder="kyats"
                                                 outlined
@@ -344,14 +370,30 @@ export default {
                 fee: { kyat: "", pal: "", yway: "" },
                 fee_price: "",
                 fee_for_making: "",
-
-                fee_for_making: "",
                 item_discount: "",
                 tax: "",
                 item_description: "",
+                total_kyat: "",
+                total_pal: "",
+                exceed_pal_form_yway: "",
+                total_yway: "",
+                total_before: "",
+                total_after: "",
+                paid_money: "",
+                credit_money: "",
             }),
             quality: "",
         };
+    },
+    methods: {
+        addToCart() {
+            let item = [];
+            item.id = this.form.id;
+            item.name = this.form.name;
+            item.image = this.form.image;
+            item.total_after = this.form.total_after;
+            this.$store.dispatch("addItem", item);
+        },
     },
 
     computed: {
@@ -368,6 +410,7 @@ export default {
             this.form.gem_weight.kyat = value.gem_weight.kyat;
             this.form.gem_weight.pal = value.gem_weight.pal;
             this.form.gem_weight.yway = value.gem_weight.yway;
+            this.form.gem_price = value.gem_price;
             this.form.fee.kyat = value.fee.kyat;
             this.form.fee.pal = value.fee.pal;
             this.form.fee.yway = value.fee.yway;
@@ -385,7 +428,84 @@ export default {
             let yway_p =
                 parseInt(this.form.gold_weight.yway) * parseInt(price.yway);
             this.form.gold_price = kyat_p + pal_p + yway_p;
-            return this.gold_price;
+            if (isNaN(this.form.gold_price)) this.form.gold_price = "";
+            return this.form.gold_price;
+        },
+        feePrice() {
+            let price = this.$page.props.daily_setup[this.quality];
+            let kyat_p = parseInt(this.form.fee.kyat) * parseInt(price.kyat);
+            let pal_p = parseInt(this.form.fee.pal) * parseInt(price.pal);
+            let yway_p = parseInt(this.form.fee.yway) * parseInt(price.yway);
+            this.form.fee_price = kyat_p + pal_p + yway_p;
+            if (isNaN(this.form.fee_price)) this.form.fee_price = "";
+
+            return this.form.fee_price;
+        },
+        totalKyat() {
+            this.form.total_kyat =
+                parseInt(this.form.gold_weight.kyat) +
+                parseInt(this.form.gem_weight.kyat) +
+                parseInt(this.form.fee.kyat);
+            if (isNaN(this.form.total_kyat)) this.form.total_kyat = "";
+
+            return this.form.total_kyat;
+        },
+        totalPal() {
+            this.form.total_pal =
+                parseInt(this.form.gold_weight.pal) +
+                parseInt(this.form.gem_weight.pal) +
+                parseInt(this.form.fee.pal) +
+                parseInt(this.form.exceed_pal_form_yway);
+            if (isNaN(this.form.total_pal)) {
+                this.form.total_pal = "";
+            }
+            if (this.form.total_pal >= 16) {
+                this.form.total_kyat =
+                    this.form.total_kyat + parseInt(this.form.total_pal / 16);
+                this.form.total_pal = this.form.total_pal % 16;
+            }
+            return this.form.total_pal;
+        },
+        totalYway() {
+            this.form.total_yway =
+                parseInt(this.form.gold_weight.yway) +
+                parseInt(this.form.gem_weight.yway) +
+                parseInt(this.form.fee.yway);
+            if (isNaN(this.form.total_yway)) {
+                this.form.total_yway = "";
+            }
+            if (this.form.total_yway >= 8) {
+                this.form.exceed_pal_form_yway = parseInt(
+                    this.form.total_yway / 8
+                );
+                this.form.total_yway = this.form.total_yway % 8;
+            }
+            return this.form.total_yway;
+        },
+        totalBefore() {
+            this.form.total_before =
+                parseInt(this.form.gold_price) +
+                parseInt(this.form.gem_price) +
+                parseInt(this.form.fee_price) +
+                parseInt(this.form.fee_for_making);
+
+            if (isNaN(this.form.total_before)) this.form.total_before = "";
+
+            return this.form.total_before;
+        },
+        totalAfter() {
+            this.form.total_after =
+                parseInt(this.form.total_before) -
+                parseInt(this.form.item_discount);
+            if (isNaN(this.form.total_after)) this.form.total_after = "";
+            return this.form.total_after;
+        },
+        creditMoney() {
+            this.form.credit_money =
+                parseInt(this.form.total_after) -
+                parseInt(this.form.paid_money);
+            if (isNaN(this.form.credit_money)) this.form.credit_money = "";
+            return this.form.credit_money;
         },
     },
 };
--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -4,20 +4,29 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
+    strict: true,
     state: {
         selectedItem: "",
+        carts:[],
     },
    
     mutations: {
         
         selectItem: (state, item) => {
             state.selectedItem = item;
+        },
+        addItem: (state, item) => {
+            state.carts.push(item);
         }
     },
     actions: {
        
         selectItem: (context, item) => {
             context.commit('selectItem', item);
+        },
+
+        addItem: (context, item) => {
+            context.commit('addItem', item);
         }
     }
 });

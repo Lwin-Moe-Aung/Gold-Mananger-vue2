2728e05a26b3b6060ee565feb6829c7b126386a1
Lwin-Moe-Aung <lwinmoeaung.ycc@gmail.com>
2022-07-05 18:02:19 +0630

    purchase return

--- resources/js/constant.js
+++ resources/js/constant.js
@@ -1,11 +1,11 @@
-// const URL = "http://54.92.8.141/pos/";
-// const IMAGE_URL = "http://54.92.8.141/";
-// const DOMAIN = "http://54.92.8.141/";
+const URL = "http://54.92.8.141/pos/";
+const IMAGE_URL = "http://54.92.8.141/";
+const DOMAIN = "http://54.92.8.141/";
 
 
-const URL = "http://localhost:8000/pos/";
-const IMAGE_URL = "http://localhost:8000/";
-const DOMAIN = "http://localhost:8000/";
+// const URL = "http://localhost:8000/pos/";
+// const IMAGE_URL = "http://localhost:8000/";
+// const DOMAIN = "http://localhost:8000/";
 
 export default {
     URL: URL,

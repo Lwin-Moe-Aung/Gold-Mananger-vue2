61a22d42bef232b74fd17ef63bb4c593dfc93c74
Lwin-Moe-Aung <lwinmoeaung.ycc@gmail.com>
2022-08-17 21:09:45 +0630

    create view table for cash in and cash out

---
+++ app/Models/ViewCashOutData.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ViewCashOutData extends Model
+{
+    use HasFactory;
+    public $table = "view_cashout_data";
+
+}

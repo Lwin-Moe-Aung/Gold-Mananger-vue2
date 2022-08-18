b4796a206285ac16b03981df6dfa91e61365750e
Lwin-Moe-Aung <lwinmoeaung.ycc@gmail.com>
2022-08-18 10:26:49 +0630

    constant file

--- resources/js/constant.js
+++ resources/js/constant.js
@@ -1,13 +1,13 @@
-// const ROUTE_URL_POS = "http://52.199.187.88/pos/";
-// const ROUTE_URL_ADMIN = "http://52.199.187.88/";
-// const IMAGE_URL = "http://52.199.187.88/";
-// const DOMAIN = "http://52.199.187.88/";
+const ROUTE_URL_POS = "http://52.199.187.88/pos/";
+const ROUTE_URL_ADMIN = "http://52.199.187.88/admin/";
+const IMAGE_URL = "http://52.199.187.88/";
+const DOMAIN = "http://52.199.187.88/";
 
 
-const ROUTE_URL_POS = "http://localhost:8000/pos/";
-const ROUTE_URL_ADMIN = "http://localhost:8000/admin/";
-const IMAGE_URL = "http://localhost:8000/";
-const DOMAIN = "http://localhost:8000/";
+// const ROUTE_URL_POS = "http://localhost:8000/pos/";
+// const ROUTE_URL_ADMIN = "http://localhost:8000/admin/";
+// const IMAGE_URL = "http://localhost:8000/";
+// const DOMAIN = "http://localhost:8000/";
 
 export default {
     ROUTE_URL_POS: ROUTE_URL_POS,

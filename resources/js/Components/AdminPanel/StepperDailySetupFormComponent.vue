748510befa6cf05d85a5d7384d75bb492ece71b1
Lwin-Moe-Aung <lwinmoeaung.ycc@gmail.com>
2022-09-26 10:01:58 +0630

    layout on stepper form

--- resources/js/Components/AdminPanel/StepperDailySetupFormComponent.vue
+++ resources/js/Components/AdminPanel/StepperDailySetupFormComponent.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>stepper daily setup form</h1>
+   <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col
+        v-for="n in 3"
+        :key="n"
+        cols="12"
+        sm="4"
+      >
+        <v-card
+          class="pa-2"
+          outlined
+          tile
+        >
+          One of three columns
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
